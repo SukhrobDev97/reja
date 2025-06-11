17afe67ca2fc4181c49649d7bdf13bfeb063fd3c
SukhrobDev97 <sukhrobDev97@gmail.com>
2025-06-12 00:30:51 +0900

    feat: frontEnd JS building

--- app.js
+++ app.js
@@ -33,12 +33,8 @@ app.post("/create-item", function (req, res) {
     console.log("user entered / create-item ")
     const new_reja = req.body.reja;
     db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
-        if (err) {
-            console.log(err);
-            res.end('smth went wrong')
-        } else {
-            res.end('successfully added')
-        }
+        console.log(data.ops);
+        res.json(data.ops[0])
     })
 });
 
